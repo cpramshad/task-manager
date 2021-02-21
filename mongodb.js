02ec2a9d0d1f174acc3b0e4ec97d37816ad339c4
cpramshad <cpramshad@gmail.com>
2021-02-21 19:17:03 +0400

    [feature] - 75 - Inserting collections and documents

--- mongodb.js
+++ mongodb.js
@@ -9,5 +9,10 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     if(error) {
         return console.log('Unable to connect to DB!');
     }
-    console.log('Connected Successfully !');
-})
+    
+    const db = client.db(databaseName);
+    db.collection('users').insertOne({
+        name: 'Ramshad',
+        age: 36,
+    });
+});
