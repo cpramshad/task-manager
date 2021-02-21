f0e913996c1fe438b19e96fa11e9d6e3b791c533
cpramshad <cpramshad@gmail.com>
2021-02-21 20:16:33 +0400

    [feature] - 76 - Inserting many documents

--- mongodb.js
+++ mongodb.js
@@ -11,14 +11,33 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     }
     
     const db = client.db(databaseName);
-    db.collection('users').insertOne({
-        name: 'Ramshad',
-        age: 36,
-    }, (error, result) => {
-        if(error) {
-            return console.log('Unable to insert user');
-        }
+    // db.collection('users').insertOne({
+    //     name: 'Ramshad',
+    //     age: 36,
+    // }, (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert user');
+    //     }
+    //     console.log(result.ops);
+    // });
 
+    db.collection('tasks').insertMany([
+        {
+            description: 'study',
+            completed: false 
+        },
+        {
+            description: 'excercise',
+            completed: true
+        },
+        {
+            description: 'work',
+            completed: true
+        }
+    ], (error, result) => {
+        if(error) {
+            return console.log('Unable to insert tasks');
+        }
         console.log(result.ops);
-    });
+    })
 });
