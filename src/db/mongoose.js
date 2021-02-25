66d3d5518c2fa72f08ba3bfc9649c64c702e18ab
cpramshad <cpramshad@gmail.com>
2021-02-25 11:52:54 +0400

    [feature] - 84-86 - Data validation and sanitizaton mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,20 +8,74 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Invalid email');
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age should be +ve');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Shouldnt contain "password"')
+            }
+        }
     }    
 });
 
-const me = new User({
-    name: 'Ramshad',
-    age: 27
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 });
 
-me.save().then(() => {
-    console.log(me);
+const newTask = new Task({
+    description: 'Workout'
+});
+
+newTask.save().then(() => {
+    console.log(newTask);
 }).catch(error => {
     console.log(error);
 });
+
+// const me = new User({
+//     name: '   Ramshad   ',
+//     email: ' CPramshad@gmail.com ',
+//     age: 36,
+//     password: 'PASsword'
+// });
+
+// me.save().then(() => {
+//     console.log(me);
+// }).catch(error => {
+//     console.log(error);
+// });
