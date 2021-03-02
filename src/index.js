99c5f2f3304e7f34b20abe90edea36b5d3f39db2
cpramshad <cpramshad@gmail.com>
2021-03-02 12:58:12 +0400

    [feature] - 90,91 - Resource Reading REST API endpoints for users

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.port || 3000;
@@ -10,8 +11,37 @@ app.use(express.json());
 app.post('/users', (req, res) => {
     const user = new User(req.body);
     user.save().then(() => {
-        res.send(user);
+        res.status(201).send(user);
     }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users);
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+    User.findById(_id).then(user => {
+        if(!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    }).catch(e => {
+        res.status(500).send(e);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+    task.save().then(() => {
+        res.status(201).send(task);
+    }).catch(e => {
         res.status(400).send(e);
     });
 });
