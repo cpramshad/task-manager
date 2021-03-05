647dffff6b0ae8a7f7c2898a95377e5e0dc5d8c8
cpramshad <cpramshad@gmail.com>
2021-03-05 10:34:12 +0400

    [feature] - 101 - separate route files

---
+++ src/routers/task.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const Task = require('../models/task');
+const router = new express.Router();
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    try {
+        await task.save();
+        res.status(201).send(task);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(404).send();
+        }
+        return res.send(task);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+router.patch('/tasks/:id', async(req, res) => {
+    console.log('patchtasks');
+    const receivedUpdates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = receivedUpdates.every(update => allowedUpdates.includes(update));
+    console.log(isValidOperation);
+
+    if(!isValidOperation) {
+        res.status(400).send({error: "Invalid Updates!"});
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+        console.log(task);
+        if(!task) {
+            res.status(404).send();
+        }
+        res.send(task);
+
+    } catch(e) {
+        res.status(400).send(e);
+    }
+});
+
+router.delete('/tasks/:id', async(req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if(!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch(e) {
+        res.status(500).send(e);
+    }
+});
+
+module.exports = router;
